eaf1304657f721f35a7207d7f3fdcfaec578698b
tharun <tharunkumar8309@gmail.com>
2025-05-17 19:16:11 +0530

    modified package.json

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/react-calculator/',
+   base: '/React_Calculator/',
   plugins: [react(),tailwindcss()],
 })
